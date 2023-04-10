864e33507ec7eb90edcc8f4c7a9588f91fbf7410
tuhin <tuhin153695@gmail.com>
2023-04-10 13:29:10 +0600

    initial data loaded

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>Order Review Page</h3>
+        </div>
+    );
+};
+
+export default OrderReview;
